a3dd651f3a392f83fed23c35c48d7143905a48ef
Rumpels95 <joan.quispec@gmail.com>
2022-04-29 03:16:25 -0500

    tercer commit

--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -1,18 +1,29 @@
 const { Router } = require('express');
-const { Recipe } = require('../db');
+const { Recipe, Diet } = require('../db');
 const router = Router();
 
 
 router.post('/', async (req,res)=> {
-  const {name, summary, aggregateLikes, healthScore, instructions} = req.body
+  const {name, summary, spoonacularScore, healthScore, instructions, diet} = req.body
   console.log(req.body)
   if(!name || !summary){
     return res.status(402).send('Completar los campos obligatorios')
   }
   
   try{
-    newRecipe = await Recipe.create(req.body)
+    newRecipe = await Recipe.create({
+      name,
+      summary,
+      spoonacularScore,
+      healthScore,
+      instructions
+    })
     console.log(newRecipe.id)
+    let dietDb = await Diet.findAll({
+      where: { name: diet }
+    })
+    newRecipe.addDiet(dietDb)
+
     res.status(202).send(req.body)
   }catch (err){
     res.status(404).send('Error en alguno de los datos provistos')
