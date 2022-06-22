1333715253f83b987e92dfefa1fb789667ead6c1
Rumpels95 <joan.quispec@gmail.com>
2022-06-21 22:03:07 -0500

    heroku

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store/index';
 import './normalize.css'
+import dotenv from 'dotenv'
+import axios from 'axios';
+dotenv.config()
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 
 ReactDOM.render(
   <Provider store={store}>
